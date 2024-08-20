e8041c920eed69121c008f19ffc04aac336eb73b
Nikhilesh <nikhileshk002@gmail.com>
2024-08-21 00:07:48 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Home from './components/Home';
+import about from './components/about';
+import Contact from './components/Contact';
+import Loginform from './components/Loginform';
+import Signup from './components/Signup';
+import {BrowserRouter as Router,Routes,Route} from "react-router-dom"
+import Navbar from './components/NAvbar';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Router>
+        <Navbar/>
+        <Routes>
+          <Route path='/' element={<Home/>}/>
+          <Route path='/Contact' element={<Contact/>}/>
+          <Route path='/About' element={<Contact/>}/>
+          <Route path='/Loginform' element={<Loginform/>}/>
+          <Route path='/Signup' element={<Signup/>}/>
+          <Route path='/Navbar' element={<Navbar/>}/>
+        </Routes>
+      </Router>
     </div>
   );
 }
---
+++ src/components/NAvbar.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const NAvbar = () => {
+  return (
+    <div>
+      <div classNAme="navbar_div">
+        <div>
+          <input type="text" placeholder='Search here'/>
+        </div>
+        <div>
+          <div className='navbar_main'>
+            <div>
+              <div>
+                {/* search icon*/}
+              </div>
+              <div>
+                {/*MenuIcon*/}
+              </div>
+              <div>
+                
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+       <Link to="/">Home</Link>
+       <Link to="/">Contacts</Link>
+       <Link to="/">Login</Link>
+       <Link to="/">Signup</Link>
+    </div>
+  )
+}
+
+export default NAvbar
